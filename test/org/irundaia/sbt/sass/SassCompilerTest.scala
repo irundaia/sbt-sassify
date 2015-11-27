552909d209a930a84319facb391ce5a9f3ee1463
Han van Venrooij <irundaia@gmail.com>
2015-11-28 00:06:15 +0100

    Restructure test

--- test/org/irundaia/sbt/sass/SassCompilerTest.scala
+++ test/org/irundaia/sbt/sass/SassCompilerTest.scala
@@ -2,50 +2,74 @@ package org.irundaia.sbt.sass
 
 import java.io.File
 
+import org.irundaia.sbt.sass.{CompilerSettings, SassCompilerException, SassCompiler}
 import org.scalatest.{FunSpec, MustMatchers}
 
 import scala.io.Source
+import scala.util.Try
 
 class SassCompilerTest extends FunSpec with MustMatchers {
-  describe("SassCompiler") {
+
+  val compilerSettings = CompilerSettings(Minified, true, true, Auto)
+
+  describe("The SassCompiler") {
     describe("using well formed scss input") {
       describe("without includes") {
+        val input = new File(getClass.getResource("/org/irundaia/sbt/sass/well-formed.scss").toURI)
+        val output = File.createTempFile("sbt-sass-test", ".css")
+        val map = File.createTempFile("sbt-sass-test", ".css.map.json")
+
+        val compilationResults = Try(new SassCompiler(compilerSettings)
+          .doCompile(input, output, map, "", ""))
+        val cssMin = Source.fromFile(output).mkString
+
+        val testMinCss = cssMin.replaceAll("\\/\\*.*?\\*\\/", "").replaceAll("\\s+", "")
+
         it("should compile") {
-          val input = new File(getClass.getResource("/org/irundaia/sbt/sass/well-formed.scss").toURI)
-          val outputMinified = File.createTempFile("sbt-sass-test", ".css")
-          val mapMinified = File.createTempFile("sbt-sass-test", ".css.map")
+          compilationResults.isSuccess mustBe true
+        }
 
-          val processOutput = new SassCompiler(CompilerSettings(Minified, true, true))
-            .doCompile(input, outputMinified, mapMinified, "", "")
-          val cssMin = Source.fromFile(outputMinified).mkString
-
-          val testMinCss = cssMin.replaceAll("\\/\\*.*?\\*\\/", "").replaceAll("\\s+", "")
+        it("should contain the proper contents") {
           testMinCss must include(".test{font-size:10px")
           testMinCss must include(".test.hidden{display:none")
+        }
 
-          processOutput.filesRead.size must be(1)
-          processOutput.filesRead.head.getCanonicalPath must include("well-formed.scss")
+        it("should have read one file") {
+          compilationResults.get.filesRead.size must be(1)
+        }
+
+        it("should have read the correct file") {
+          compilationResults.get.filesRead.head.getCanonicalPath must include("well-formed.scss")
         }
       }
 
       describe("with includes") {
+        val input = new File(getClass.getResource("/org/irundaia/sbt/sass/well-formed-using-import.scss").toURI)
+        val output = File.createTempFile("sbt-sass-test", ".css")
+        val map = File.createTempFile("sbt-sass-test", ".css.map.json")
+
+        val compilationResult = Try(new SassCompiler(compilerSettings)
+          .doCompile(input,  output, map, "", ""))
+
+        val cssMin = Source.fromFile(output).mkString
+        val testMinCss = cssMin.replaceAll("\\/\\*.*?\\*\\/", "").replaceAll("\\s+", "")
+
         it("should compile") {
-          val input = new File(getClass.getResource("/org/irundaia/sbt/sass/well-formed-using-import.scss").toURI)
-          val outputMinified = File.createTempFile("sbt-sass-test", ".css")
-          val mapMinified = File.createTempFile("sbt-sass-test", ".css.map")
+          compilationResult.isSuccess mustBe true
+        }
 
-          val processOutput = new SassCompiler(CompilerSettings(Minified, true, true))
-            .doCompile(input,  outputMinified, mapMinified, "", "")
-
-          val cssMin = Source.fromFile(outputMinified).mkString
-
-          val testMinCss = cssMin.replaceAll("\\/\\*.*?\\*\\/", "").replaceAll("\\s+", "")
+        it("should include the contents of both the included and the including file") {
           testMinCss must include(".test-import{font-weight:bold")
           testMinCss must include(".test{font-size:10px")
           testMinCss must include(".test.hidden{display:none")
+        }
 
-          processOutput.filesRead.size must be(2)
-          processOutput.filesRead.map(_.getCanonicalPath).find(_.contains("_well-formed-import.scss")) must not be None
+        it("should have read two files") {
+          compilationResult.get.filesRead.size must be(2)
+        }
+
+        it("should have read the included file") {
+          compilationResult.get.filesRead.map(_.getCanonicalPath).find(_.contains("_well-formed-import.scss")) must not be None
         }
       }
     }
@@ -53,14 +77,14 @@ class SassCompilerTest extends FunSpec with MustMatchers {
     describe("using broken scss input") {
       it("should throw an exception") {
         val input = new File(getClass.getResource("/org/irundaia/sbt/sass/broken-input.scss").toURI)
-        val outputMinified = File.createTempFile("sbt-sass-test", ".css")
-        val mapMinified = File.createTempFile("sbt-sass-test", ".css.map")
+        val output = File.createTempFile("sbt-sass-test", ".css")
+        val map = File.createTempFile("sbt-sass-test", ".css.map.json")
 
-        val exception = the [SassCompilerException] thrownBy
-          new SassCompiler(CompilerSettings(Minified, true, true))
-            .doCompile(input, outputMinified, mapMinified, "", "")
+        val exception = the[SassCompilerException] thrownBy
+          new SassCompiler(compilerSettings)
+            .doCompile(input, output, map, "", "")
 
-        exception.getMessage must include("invalid property name")
+        exception.problem.message mustEqual "invalid property name"
       }
     }
   }
