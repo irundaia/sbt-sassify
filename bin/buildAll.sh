a3d436ab4d55304756ab8214631d4d47f9776296
Han van Venrooij <han.van.venrooij@icloud.com>
2019-04-06 15:37:00 +0200

    Update binaries to 4.5.5

--- bin/buildAll.sh
+++ bin/buildAll.sh
@@ -2,6 +2,11 @@
 
 docker run --rm -v $(pwd)/bin/docker/buildWin32/x86/:/Compile -v $(pwd):/sass libsass/windows:1
 docker run --rm -v $(pwd)/bin/docker/buildWin32/x86-64/:/Compile -v $(pwd):/sass libsass/windows:1
-docker run --rm -v $(pwd)/bin/docker/buildLinux/x86/:/Compile -v $(pwd):/sass libsass/linux32:1
+# docker run --rm -v $(pwd)/bin/docker/buildLinux/x86/:/Compile -v $(pwd):/sass libsass/linux32:1
+
+./bin/docker/buildLinux/x86/dockcross make -C src/native/ -j8 shared
+cp src/native/lib/libsass.so src/main/resources/linux-x86/libsass.so
+./bin/docker/buildLinux/x86/dockcross make -C src/native/ clean
+
 docker run --rm -v $(pwd)/bin/docker/buildLinux/x86-64/:/Compile -v $(pwd):/sass libsass/linux:1
 docker run --rm -v $(pwd)/bin/docker/buildDarwin:/Compile -v $(pwd):/sass libsass/darwin:1
