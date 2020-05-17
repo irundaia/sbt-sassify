3af1722360da560389ac85fe7c5bbcab69398fb3
Han van Venrooij <han.van.venrooij@icloud.com>
2020-05-17 23:45:48 +0200

    Bump libsass version to 4.6.4

--- bin/buildAll.sh
+++ bin/buildAll.sh
@@ -2,7 +2,6 @@
 
 docker run --rm -v $(pwd)/bin/docker/buildWin32/x86/:/Compile -v $(pwd):/sass libsass/windows:1
 docker run --rm -v $(pwd)/bin/docker/buildWin32/x86-64/:/Compile -v $(pwd):/sass libsass/windows:1
-# docker run --rm -v $(pwd)/bin/docker/buildLinux/x86/:/Compile -v $(pwd):/sass libsass/linux32:1
 
 ./bin/docker/buildLinux/x86/dockcross make -C src/native/ -j8 shared
 cp src/native/lib/libsass.so src/main/resources/linux-x86/libsass.so
